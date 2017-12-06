926c07a5633341accfc2f6ed4900960f61543d63
Corona Tsai <cstsai2@illinois.edu>
2017-12-06 01:17:33 -0600

    Started writing player class

--- MP7/src/Player.java
+++ MP7/src/Player.java
@@ -1,4 +1,32 @@
-
+/**
+ * A class that implements a player for CandyLand.
+ * @author Corona & Andrea
+ */
 public class Player {
-
+	private String name;
+	private int age;
+	
+	public Player(String name, int age) {
+		this.name = name;
+		this.age = age;
+	}
+	
+	public int getAge() {
+		return this.age;
+	}
+	
+	public void setAge(int newAge) {
+		this.age = newAge;
+	}
+	/**
+	 * Returns the player name.
+	 * @return the current player's name
+	 */
+	public String getName() {
+		return this.name;
+	}
+	
+	public void setName(String newName) {
+		this.name = newName;
+	}
 }
