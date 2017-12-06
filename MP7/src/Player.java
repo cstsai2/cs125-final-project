8e0189da2aea058dd970f6248cedfb3c8468c28b
Corona Tsai <cstsai2@illinois.edu>
2017-12-06 01:29:43 -0600

    Completed player class

--- MP7/src/Player.java
+++ MP7/src/Player.java
@@ -3,30 +3,87 @@
  * @author Corona & Andrea
  */
 public class Player {
+	/** Player name. */
 	private String name;
+	/** Player age. */
 	private int age;
+	/** Tracks player position. */
+	private int position;
+	/** Tracks if player will have a turn skipped.*/
+	private boolean isSkipped;
 	
+	/**
+	 * Empty Constructor.
+	 */
+	public Player() {
+		
+	}
+	/**
+	 * Creates a new player with a given name and age.
+	 * Each player begins at position 0 and does not have their turn skipped.
+	 * @param name - the name for the new player
+	 * @param age - the age for the new player
+	 */
 	public Player(String name, int age) {
 		this.name = name;
 		this.age = age;
+		this.position = 0;
+		this.isSkipped = false;
 	}
-	
+	/**
+	 * Get the player's age.
+	 * @return the player's age
+	 */
 	public int getAge() {
 		return this.age;
 	}
-	
+	/**
+	 * Set the player's age.
+	 * @param newAge - the player's new age
+	 */
 	public void setAge(int newAge) {
 		this.age = newAge;
 	}
 	/**
-	 * Returns the player name.
-	 * @return the current player's name
+	 * Gets the player name.
+	 * @return the player's name
 	 */
 	public String getName() {
 		return this.name;
 	}
-	
+	/**
+	 * Set the player's name.
+	 * @param newName - the player's new name
+	 */
 	public void setName(String newName) {
 		this.name = newName;
+	}
+	/**
+	 * Gets the player's position.
+	 * @return the player position
+	 */
+	public int getPosition() {
+		return this.position;
+	}
+	/**
+	 * Sets the player's position.
+	 * @param newPosition - the new player position
+	 */
+	public void setPosition(int newPosition) {
+		this.position = newPosition;
+	}
+	/**
+	 * Gets whether or not the player's next turn will be skipped.
+	 * @return true if player's next turn should be skipped, else false
+	 */
+	public boolean getSkipCondition() {
+		return this.isSkipped;
+	}
+	/**
+	 * Sets player's 'isSkipped' condition.
+	 * @param condition - the player's new condition
+	 */
+	public void setSkipCondition(boolean condition) {
+		this.isSkipped = condition;
 	}
 }
