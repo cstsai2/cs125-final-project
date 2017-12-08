41a4d9dd2641bac049506a94703f9130d3b30319
Corona Tsai <cstsai2@illinois.edu>
2017-12-08 17:48:31 -0600

    Updated the Position class to encompass the special conditions

--- MP7/src/Position.java
+++ MP7/src/Position.java
@@ -6,6 +6,8 @@
 public class Position {
 
 	private String color;
+	private boolean isBridge;
+	private boolean isSticky;
 	/** 
 	 * Empty Constructor.
 	 */
@@ -18,6 +20,35 @@ public class Position {
 	 */
 	public Position(String color) {
 		this.color = color.toUpperCase();
+		isBridge = false;
+	}
+	/**
+	 * Set position to be a bridge.
+	 * Position 5 connects to 59 and position 35 to 46.
+	 */
+	public void setBridge(boolean condition) {
+		isBridge = condition;
+	}
+	/**
+	 * Get the isBridge status of the position.
+	 * @return true if it is a bridge, false otherwise
+	 */
+	public boolean getBridge() {
+		return isBridge;
+	}
+	/**
+	 * Set position to be a "sticky" space.
+	 * Positions 47, 76, and 89 are sticky spaces.
+	 */
+	public void setSticky(boolean condition) {
+		isSticky = condition;
+	}
+	/**
+	 * Get the isSticky status of the position.
+	 * @return true if it is a sticky position, false otherwise
+	 */
+	public boolean getSticky() {
+		return isSticky;
 	}
 	/**
 	 * Get the color of the square.
