0f80d3b50d9c29192e4a547086a67b21e31aba49
Corona Tsai <cstsai2@illinois.edu>
2017-12-10 16:45:47 -0600

    Complete w a small bug

--- MP7/src/CandyLandDemo.java
+++ MP7/src/CandyLandDemo.java
@@ -20,12 +20,15 @@ public class CandyLandDemo {
 			if (!players[playerTurn].getSkipCondition()) {
 				
 				System.out.println("It is now " + players[playerTurn].getName() + "'s turn.");
+				
 				turn(players[playerTurn]); //problem
 				isWinnerFound = checkWinner();
-				
+				System.out.println(" ");
+				printStatus();
 			} else { 
 				
 				System.out.println(players[playerTurn].getName() + "'s turn has been skipped.");
+				System.out.println(players[playerTurn].getName() + " does not advance.");
 				players[playerTurn].setSkipCondition(false);
 				
 			}
@@ -34,7 +37,7 @@ public class CandyLandDemo {
 			if (playerTurn > players.length - 1) { //if the last player has just taken their turn
 				playerTurn = 0;						// it wraps back around to the first player
 			}
-			printStatus();
+			
 			System.out.println("*******************************");
 			try {
 				Thread.sleep(1000);
@@ -47,12 +50,12 @@ public class CandyLandDemo {
 	}
 	
 	/**
-	 * Checks if any player has reached position 135. 135 is the position of the finish.
+	 * Checks if any player has reached position 134. 134 is the position of the finish.
 	 * @return true if above condition is found, false otherwise
 	 */
 	private static boolean checkWinner() {
 		for (Player p : players) {
-			if (p.getPosition() == 135) {
+			if (p.getPosition() == 134) {
 				winner = new Player(p);
 				return true;
 			}
@@ -69,10 +72,24 @@ public class CandyLandDemo {
 		String card = myCard.draw();
 		System.out.println(player.getName() + " has drawn a card '" + card + "'." );
 		if (card.length() <= 2) { //if a color or double color card has been drawn
-			move(player, card); //problem
-		} //else move to other specified location
-		
-		//...
+			move(player, card); 
+			System.out.print(player.getName() + " has moved.");
+		} else {//a character card has been drawn
+			//All the character cards
+			int[] characters = {9, 20, 42, 69, 92, 102};
+			//Character card names
+			String[] characterNames = {"Gramma Nut",  "Queen Frostine", "Mr. Mint", "Candy Princess", "Gumdrop", "Candy Cane"};
+			int newPosition = 0;
+			for (int i = 0; i < 6; i++) {
+				if (card.equals(characterNames[i])) {
+					newPosition = characters[i];
+					break;
+				}
+			}
+			player.setPosition(newPosition);
+			System.out.println(player.getName() + " has moved to position " + newPosition);
+			
+		}
 	}
 	/**
 	 * A helper method for move().
@@ -82,13 +99,13 @@ public class CandyLandDemo {
 	 * @return the next closest position when the space is the color of interest, or finish if no more colors
 	 */
 	private static int getNextPosition(int position, String color) {
-		for (int i = position + 1; i < 135; i++) {
+		for (int i = position + 1; i < 134; i++) {
 			String pColor = board.getPositionColor(i);
 			if (pColor.equals(color)) {
 				return i;
 			}
 		}
-		return 135;
+		return 134;
 	}
 	/**
 	 * Move the player to the specified color space on the card drawn.
@@ -100,21 +117,26 @@ public class CandyLandDemo {
 		int currentPosition = player.getPosition();
 		String color = card.substring(0,1);
 		endPosition = getNextPosition(currentPosition, color); //problem
- 		if (card.length() == 2) { //this is a double color card
+ 		if (card.length() == 2 && endPosition != 134) { //this is a double color card
 			endPosition = getNextPosition(endPosition, color);
-		}
-		
-		if (board.getBridge(endPosition)) { // player has landed on a bridge
-			System.out.println(player.getName() + " has landed on a bridge.");
-			//set endPosition to something else
 		}
 		
 		if (board.getSticky(endPosition)) { //the player has landed on a "sticky" space
 			System.out.println(player.getName() + " has landed on a sticky space.");
 			player.setSkipCondition(true); // Player will have to skip a turn
 		}
+		if (board.getBridge(endPosition)) {
+			System.out.println(player.getName() + "has landed on a bridge.");
+			if (endPosition == 5) {
+				endPosition = 59;
+				System.out.print(player.getName() + " crosses the bridge and lands at position 59.");
+			} else if (endPosition  == 35) {
+				endPosition = 46;
+				System.out.print(player.getName() + " crosses the bridge and lands at position 46.");
+			}
+		}
 		player.setPosition(endPosition);
-		//dont forget about bridges!
+
 	}
 	/**
 	 * Print the current player positions.
