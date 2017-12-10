67c5ac9a7567e8e096a44870237bb8091c59219e
Corona Tsai <cstsai2@illinois.edu>
2017-12-10 15:54:56 -0600

    Completed

--- MP7/src/GameBoard.java
+++ MP7/src/GameBoard.java
@@ -16,12 +16,6 @@ public class GameBoard {
 	public GameBoard() {
 		//initialize board
 		board = new Position[NUMPOSITIONS];
-		setBoard();
-	}
-	/**
-	 * Sets up the board.
-	 */
-	public void setBoard() {
 		//All the red spaces
 		int[] red = {1, 7, 14, 21, 27, 33, 39, 46, 52, 64, 71, 77, 83, 89, 96, 103, 109, 115, 121, 127, 133};
 		//All the orange spaces
@@ -34,35 +28,67 @@ public class GameBoard {
 		int[] blue = {4, 11, 17, 24, 30, 36, 43, 49, 55, 61, 67, 74, 80, 86, 93, 99, 106, 112, 118, 124, 130};
 		//All the purple spaces
 		int[] purple = {2, 8, 15, 22, 28, 34, 40, 47, 53, 59, 65, 72, 78, 84, 90, 97, 104, 110, 116, 122, 128};
+		//All the character cards
+		int[] characters = {9, 20, 42, 69, 92, 102};
 		//All the "sticky" spaces
 		int[] sticky = {47, 76, 89};
 		//All the bridges
 		int[] bridge = {5, 35};
 		
 		
-		for (int i = 1; i < 135; i++) { //index 0 is start, index 135 is finish
-			int temp = i;
-			//Set the board colors
-			if (IntStream.of(red).anyMatch(x -> x == temp)) {
-				board[i] = new Position("r");
-			} else if (IntStream.of(orange).anyMatch(x -> x == temp)) {
-				board[i] = new Position("o");
-			} else if (IntStream.of(yellow).anyMatch(x -> x == temp)) {
-				board[i] = new Position("y");
-			} else if (IntStream.of(green).anyMatch(x -> x == temp)) {
-				board[i] = new Position("g");
-			} else if (IntStream.of(blue).anyMatch(x -> x == temp)) {
-				board[i] = new Position("b");
-			} else if (IntStream.of(purple).anyMatch(x -> x == temp)) {
-				board[i] = new Position("p");
-			}
-			//Set special conditions
-			if (IntStream.of(bridge).anyMatch(x -> x==temp)) {
-				board[i].setBridge(true);
-			} else if (IntStream.of(sticky).anyMatch(x -> x == temp)) {
-				board[i].setSticky(true);
-			}
+		for (int i : red) {
+			board[i] = new Position("r");
 		}
+		for (int i : orange) {
+			board[i] = new Position("o");
+		}
+		for (int i : yellow) {
+			board[i] = new Position("y");
+		}
+		for (int i : green) {
+			board[i] = new Position("g");
+		}
+		for (int i : blue) {
+			board[i] = new Position("b");
+		}
+		for (int i : purple) {
+			board[i] = new Position("p");
+		}
+		for (int i : characters) {
+			board[i] = new Position("character card");
+		}
+		for (int i : sticky) {
+			board[i].setSticky(true);
+		}
+		for (int i : bridge) {
+			board[i].setBridge(true);
+		}
+		
+//		for (int i = 1; i < 135; i++) { //index 0 is start, index 135 is finish
+//			int temp = i;
+//			//Set the board colors
+//			if (IntStream.of(red).anyMatch(x -> x == temp)) {
+//				board[i] = new Position("r");
+//			} else if (IntStream.of(orange).anyMatch(x -> x == temp)) {
+//				board[i] = new Position("o");
+//			} else if (IntStream.of(yellow).anyMatch(x -> x == temp)) {
+//				board[i] = new Position("y");
+//			} else if (IntStream.of(green).anyMatch(x -> x == temp)) {
+//				board[i] = new Position("g");
+//			} else if (IntStream.of(blue).anyMatch(x -> x == temp)) {
+//				board[i] = new Position("b");
+//			} else if (IntStream.of(purple).anyMatch(x -> x == temp)) {
+//				board[i] = new Position("p");
+//			}
+//			//Set special conditions
+//			if (IntStream.of(bridge).anyMatch(x -> x==temp)) {
+//				board[i].setBridge(true);
+//			} else if (IntStream.of(sticky).anyMatch(x -> x == temp)) {
+//				board[i].setSticky(true);
+//			}
+//		}
+		board[0] = new Position("start");
+		board[135] = new Position("finish");
 	}
 	/**
 	 * Returns the color of the specified space.
@@ -70,7 +96,7 @@ public class GameBoard {
 	 * @return the color of the space at the position
 	 */
 	public String getPositionColor(int position) {
-		return board[position].getColor();
+		return board[position].getColor(); //root of problem!!!!
 	}
 	/**
 	 * Returns the condition, sticky or not, of the specific position.
