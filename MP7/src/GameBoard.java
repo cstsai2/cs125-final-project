8fb803bf62d7ed8ac60bae6fe414b6885848ff4d
aayala32 <32136575+aayala32@users.noreply.github.com>
2017-12-12 02:16:51 -0600

    Update GameBoard.java

--- MP7/src/GameBoard.java
+++ MP7/src/GameBoard.java
@@ -14,29 +14,29 @@ public class GameBoard {
 	 * Constructor.
 	 */
 	public GameBoard() {
-		//initialize board
+		// initialize board
 		board = new Position[NUMPOSITIONS];
-		//All the red spaces
+		// All the red spaces
 		int[] red = {1, 7, 14, 21, 27, 33, 39, 46, 52, 58, 64, 71, 77, 83, 89, 96, 103, 109, 115, 121, 127, 133};
-		//All the orange spaces
+		// All the orange spaces
 		int[] orange = {5, 12, 18, 25, 31, 37, 44, 50, 56, 62, 68, 75, 81, 87, 94, 100, 107, 113, 119, 125, 131};
-		//All the yellow spaces
+		// All the yellow spaces
 		int[] yellow = {3, 10, 16, 23, 29, 35, 41, 48, 54, 60, 66, 73, 79, 85, 91, 98, 105, 111, 117, 123, 129};
-		//All the green spaces
+		// All the green spaces
 		int[] green = {6, 13, 19, 26, 32, 38, 45, 51, 57, 63, 70, 76, 82, 88, 95, 101, 108, 114, 120, 126, 132};
-		//All the blue spaces
+		// All the blue spaces
 		int[] blue = {4, 11, 17, 24, 30, 36, 43, 49, 55, 61, 67, 74, 80, 86, 93, 99, 106, 112, 118, 124, 130};
-		//All the purple spaces
+		// All the purple spaces
 		int[] purple = {2, 8, 15, 22, 28, 34, 40, 47, 53, 59, 65, 72, 78, 84, 90, 97, 104, 110, 116, 122, 128};
-		//All the character cards
+		// All the character cards
 		int[] characters = {9, 20, 42, 69, 92, 102};
-		//Character card names
+		// Character card names
 		String[] characterNames = {"Gramma Nut",  "Queen Frostine", "Mr. Mint", "Candy Princess", "Gumdrop", "Candy Cane"};
 		
 		
-		//All the "sticky" spaces
+		// All the "sticky" spaces
 		int[] sticky = {47, 76, 89};
-		//All the bridges
+		// All the bridges
 		int[] bridge = {5, 35};
 		
 		
